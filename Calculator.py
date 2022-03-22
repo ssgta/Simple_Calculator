1aa7a4eb0566369b88dc4fae15f7ab38b98380cb
ssgta <62765221+ssgta@users.noreply.github.com>
2022-03-22 22:54:05 +0000

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,45 @@
+# A simple calculator for addition, subtraction, multiplication, and division.
+
+def main():
+    # Initialise sum as first number input
+    while True:
+        try:
+            sum = float(input("Number: "))
+
+        except ValueError:
+            print("Please type a number.")
+
+        else:
+            break
+
+    # Until '=' operator is sent, continue performing calculation
+    while True:
+        operator = input("Operator (+ - / * =): ")
+        if operator == "=":
+            break
+        try:
+            num = float(input("Number: "))
+
+        except ValueError:
+            print("Please type a number.")
+
+        else:
+            match operator:
+                case "+" :
+                    sum += num
+                case "-" :
+                    sum -= num
+                case "/" :
+                    sum /= num
+                case "*" :
+                    sum *= num
+                case _:
+                    print(f"Invalid operator: {operator}")
+                    exit()
+
+    # Print final sum
+    print(sum)
+
+
+if __name__ == '__main__':
+    main()
